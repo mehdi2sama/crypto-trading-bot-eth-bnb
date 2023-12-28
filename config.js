a64f1408e2079b65e332b552ccc5cc55e7ee780a
mehdi2sama <138163095+mehdi2sama@users.noreply.github.com>
2023-12-28 13:19:29 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var myaddress = "your public address"
 var myprivatekey = "your privatekey to the address"
 var myseed = "your wallet seed" //only give this if your privatekey is stored in a wallet with no privatekey accessibility example (hardwarewallets)
-var networks = "1" //1 = ETH , 2 = BNB , 3 = POLYGON
+var networks = "1" //1 = ETH , 2 = BNB , 3 = POLYGON "your privatekey to the address"
 var maxspend = "0.5" // max eth you want to spend. Note: Make sure you have that amount in the wallet you provided.
